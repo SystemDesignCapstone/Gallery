e7dc7371d29e08f3fc0095a7808e960352c0c364
Darren Lau <darrlau@gmail.com>
2018-11-10 16:15:11 -0800

    newest guy

--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
-const app = require('./app');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const Listing = require('../database/Listing.js');
 
-const port = 1128;
+const app = express();
+const port = process.env.PORT || 1128;
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+
+app.get('/listings', (req, res) => {
+  Listing.find((err, listing) => {
+    if (err) {
+      throw err;
+    } else {
+      return res.send(listing);
+    }
+  });
+});
+
+app.get('/listings/:id', (req, res) => {
+  Listing.find({photoId: req.params.id}, (err, listing) => {
+    if (err) {
+      throw err;
+    } else {
+      return res.send(listing)
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
