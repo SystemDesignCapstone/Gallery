daf5f70204cdfbb41d2dcad90599d51432e01e31
Darren Lau <darrlau@gmail.com>
2018-11-11 10:29:00 -0800

    another tra

--- database/index.js
+++ database/index.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb+srv://airjld:asdfqwerzxcv1!@airjld-cluster-wa63e.mongodb.net/test?retryWrites=true'
+const mongoUri = 'mongodb+srv://airjld:asdfqwerzxcv1!@airjld-cluster-wa63e.mongodb.net/fec?retryWrites=true'
 
-const db = mongoose.connect(mongoUri, (err, client) => {
+const db = mongoose.connect(mongoUri, (err) => {
   if (err) { console.log(err, 'trouble connecting to DB'); }
-  const collection = client.db("fec").collection("listings");
   console.log('success connecting to DB');
-  client.close();
 })
 
 module.exports = db;
