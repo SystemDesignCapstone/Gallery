b93ad0509aea822b11481713858e6aed437a3597
Darren Lau <darrlau@gmail.com>
2018-11-11 09:32:45 -0800

    hook up db

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://localhost/a-fec';
+const mongoUri = 'mongodb+srv://airjld:asdfqwerzxcv1!@airjld-cluster-wa63e.mongodb.net/test?retryWrites=true'
 
 const db = mongoose.connect(mongoUri);
 
