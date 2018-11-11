69ae823c0f98f255375434710407acb12fabac79
Darren Lau <darrlau@gmail.com>
2018-11-11 10:25:21 -0800

    another tryyy

--- database/index.js
+++ database/index.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const mongoUri = 'mongodb+srv://airjld:asdfqwerzxcv1!@airjld-cluster-wa63e.mongodb.net/test?retryWrites=true'
 
-const db = mongoose.connect(mongoUri, (err) => {
+const db = mongoose.connect(mongoUri, (err, client) => {
   if (err) { console.log(err, 'trouble connecting to DB'); }
+  const collection = client.db("fec").collection("listings");
   console.log('success connecting to DB');
+  client.close();
 })
 
 module.exports = db;
