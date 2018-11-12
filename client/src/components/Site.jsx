0d0d81f0bb8302ba71e798b407b206927b248ef0
Darren Lau <darrlau@gmail.com>
2018-11-11 20:22:02 -0800

    uh huh v4

--- client/src/components/Site.jsx
+++ client/src/components/Site.jsx
@@ -27,17 +27,13 @@ class Site extends React.Component {
         return res.json();
       })
       .then(result => {
-        console.log(result, 'this is the result')
-        console.log(result[0].urls, 'yup 0 urls')
-        console.log(result[1].urls, 'yup 1 urls')
-        console.log(result[2].urls, 'yup 2 urls')
         this.setState({
-          mainPicture: result[0].urls[0],
-          picture1: result[0].urls[1],
-          picture2: result[0].urls[2],
-          picture3: result[0].urls[3],
-          picture4: result[0].urls[4],
-          pictures: result[0].urls
+          mainPicture: result[0].urls,
+          picture1: result[1].urls,
+          picture2: result[2].urls,
+          picture3: result[3].urls,
+          picture4: result[4].urls,
+          pictures: result[5].urls
         })
       })
   }
