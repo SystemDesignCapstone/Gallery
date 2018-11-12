a6eaeb00c7082b263f9546b7aaee523c5a9dc506
Darren Lau <darrlau@gmail.com>
2018-11-11 20:08:36 -0800

    uh huh v2

--- client/src/components/Site.jsx
+++ client/src/components/Site.jsx
@@ -24,7 +24,7 @@ class Site extends React.Component {
     const rand = Math.floor(Math.random() * 100) + 1;
     fetch(`http://airjld2-env.nhf7jyknam.us-east-2.elasticbeanstalk.com/listings/${rand}`)
       .then(res => {
-        console.log('this is the result', res)
+        console.log('this is the result', res.json())
         return res.json();
       })
       .then(result => {
